0ab92119e3aa3cef96b49e13a53cca9d90d91993
Braedon <bbanta01@icloud.com>
2024-08-24 13:09:57 -0400

    Start find the oldest exercise.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(personList) {
+    // map, sort, filter, reduce
+    const firstPerson = personList[0];
+    return personList.reduce((currentOldest, currentPerson) => {
+        let currentPersonAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+        if (currentPersonAge > currentOldest) {
+            return currentPerson;
+        }
+    }, 0);
 };
 
 // Do not edit below this line
