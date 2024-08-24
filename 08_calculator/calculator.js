39e0575918acbbecf9fa9afb8738e6694bad9bbe
Braedon <bbanta01@icloud.com>
2024-08-24 11:41:51 -0400

    Finish calculator exercise.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+  return numList.reduce((total, current) => {
+    return total + current;
+  }, 0
+)};
+
+const multiply = function(numList) {
+  return numList.reduce((total, currentNum) => {
+    return total * currentNum;
+  });
 };
 
-const multiply = function() {
-
+const power = function(num1, num2) {
+	return num1**num2
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total
 };
 
 // Do not edit below this line
